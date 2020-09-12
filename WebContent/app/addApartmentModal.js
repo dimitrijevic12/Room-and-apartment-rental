628850fa17eedcd4ac0c2445a62f1fb76a33b022
Nemanja Dimitrijevic <necapro@gmail.com>
2020-09-12 05:27:38 +0200

    Dodato uspešno čuvanje slika i approved dates parova.

--- WebContent/app/addApartmentModal.js
+++ WebContent/app/addApartmentModal.js
@@ -34,62 +34,108 @@ Vue.component('add-apartment-modal',{
 						</div>
 						<input type="number"/>
 					</div>
-					<div class="label-input-signup">
+					<div class="label-input-signup last">
 						<div class='label-error'>
 							<label>Guest count:</label>
 						</div>
 						<input type="number"/>
 					</div>
 				</div>
-				<div class="add-apartment-column">
-					<div class="label-input-signup">
+				<div class="add-apartment-column" :key="listKey">
+					<div class="label-input-signup bigger">
 					<div class='label-error'>
 						<label>Images:</label>
 					</div>
 					<input type="file" chips ref="browseImages" accept=".jpg,.png" @change="imageAdded($event)" multiple/><br/>
+					<div v-for="file in files"><label>{{file.name}}</label></div>
 				</div>
-				<div class="label-input-signup date-time-picker-container">
+				<div class="label-input-signup date-time-picker-contain">
 					<div class='label-error'>
 						<label>Check in - Check out time:</label>
 					</div>
 					<vue-ctk-date-time-picker class="timepicker" v-model="checkInTime" :label="'Check in'" :format="'HH:mm'" :formatted="'HH:mm'" :inputSize="'sm'" :onlyTime="true"></vue-ctk-date-time-picker>
 					<vue-ctk-date-time-picker class="timepicker" v-model="checkOutTime" :label="'Check out'" :format="'HH:mm'" :formatted="'HH:mm'" :inputSize="'sm'" :onlyTime="true"></vue-ctk-date-time-picker>
 				</div>
-				<div class="label-input-signup date-time-picker-container">
+				<div class="label-input-signup date-time-picker-container bigger">
 					<div class='label-error'>
 						<label>Approve 
 s</label>
 					</div>
-					<vue-ctk-date-time-picker v-model="approvedDates" :label="'Choose dates'" :format="'DD/MM/YYYY'" :formatted="'DD/MM/YYYY'" :range="true" v-bind:disabledDates="['2020-09-11','2020-09-12','2020-09-13']"></vue-ctk-date-time-picker><br>
+					<vue-ctk-date-time-picker v-model="approvedDate" :label="'Choose dates'" :format="'DD/MM/YYYY'" :formatted="'DD/MM/YYYY'" :range="true" v-bind:disabledDates="['2020-09-11','2020-09-12','2020-09-13']" @validate="addApprovedDate"></vue-ctk-date-time-picker><br>
+					<ul>
+						<li v-for="date in apartment.approvedDates">{{date.dateStart | dateFormat('DD/MM/YYYY')}} - {{date.dateEnd | dateFormat('DD/MM/YYYY')}}</li>
+					</ul>
+					
 				</div>
-				<div class="label-input-signup last">
+				<div class="label-input-signup bigger last">
 					<div class='label-error'>
-						<label>Amenities</label>
+						<label>Amenities:</label><br>
 					</div>
-						<select>
-							<option v-for="amenity in amenities" value="amenity.id">{{amenity.name}}</option>
-						</select><br/>
+						<div class="amenities-containter">
+							<table class="amenity-table">
+								<tr><th><div>All</div></th></tr>
+								<tr v-for="amenity in amenities" @click="selectedAmenity = amenity">{{amenity.name}}</tr>
+							</table>
+							<div class="amenity-button-container">
+								<button class="arrow-button" @click="addAmenity">&#8250;</button>
+								<button class="arrow-button" @click="removeAmenity">&#8249;</button>
+							</div>
+							<table class="amenity-table">
+								<tr><th><div>In apartment</div></th></tr>
+								<tr v-for="amenity in addedAmenities" @click="selectedAmenity = amenity">{{amenity.name}}</tr>
+							</table>
+						</div
 					</div>
 				</div>
 				</div>
 				<br>
-				<div class="create-button-containter">
+			</div>
+			<div class="create-button-container">
 					<button @click="test">Create</button>
-				</div>
+					<button @click="test2">test2</button>
+			</div>
 			</div>
 		</div>
 	`,
 	data: function(){
 		return{
+			apartment : {
+					type:"ROOM",
+					name:"Poslednji test pred commit",
+					roomCount:10,
+					guestCount:4,
+					location:{
+						latitude:212.0,
+						longitude:214.0,
+						address:{
+							street:"Topolska 18",
+							city:"New Now",
+							postalCode:21000
+						}
+					},
+					approvedDates:[],
+					availableDates:[],
+					hostUsername:"marija",
+					images: [],
+					price:222.0,
+					checkInTime:2323223232,
+					checkOutTime:2323223232,
+					status:"ACTIVE",
+					amenitiesIds:[],
+					id:4,
+			},
 			checkInTime: null,
 			checkOutTime: null,
-			approvedDates: {
+			approvedDate: {
 				start: '09/09/2020',
 				end: '10/09/2020'
 			},
 			amenities: null,
 			files: null,
-			image: null
+			image: null,
+			listKey: 0,
+			selectedAmenity : null,
+			addedAmenities : [],
 		}
 	},
 	mounted(){
@@ -102,67 +148,83 @@ s</label>
 		closeAddApartmentModal(){
 			this.$refs.addApartmentModal.style.display="none";
 		},
-
-		test(event){
+		test(){
 			fd = new FormData();
-//			for(i = 0; i < this.images.length; ++i){
-//				fd.append('image', this.images[i], this.images[i].name)
-//			}
-			fd.append('image123', event.target.files[0])
-			console.log(event.target.files[0]);
-			console.log(fd);
-/*			const reader = new FileReader()
-			blob = new Blob(this.files);
-			reader.readAsDataURL(blob)
-			var imageToSend
-            reader.onload = e => {
-				this.image = e.target.result
-				imageToSend = {image:e.target.result}
-                console.log(this.image)
-			}
-			console.log(this.image)
-			var apartmentObject = {
-				type:"ROOM",
-				name:"Test",
-				roomCount:10,
-				guestCount:4,
-				location:{
-					latitude:212.0,
-					longitude:214.0,
-					address:{
-						street:"Topolska 18",
-						city:"New Now",
-						postalCode:21000
-					}
-				},
-				approvedDates:[],
-				availableDates:[],
-				hostUsername:"marija",
-				images: this.image,
-				price:222.0,
-				checkInTime:2323223232,
-				checkOutTime:2323223232,
-				status:"ACTIVE",
-				amenitiesIds:[0,1,2,3],
-				id:0,
-				stars:4
-			}
-*/			
-//			console.log(imageToSend);
+			fd.append('image', this.files[0])
 			axios
 				.post('rest/apartments/images', fd)
-
-		}
-,
-		imageAdded(event){
-			fd = new FormData();
-			fd.append('image', event.target.files[0])
-			console.log(event.target.files[0]);
-			console.log(fd);
-
-			axios
-				.post('rest/apartments/images', fd)
+				.then((response) => {this.apartment.images.push(response.data); 
+									if(this.files.length > 1){
+										this.uploadImage(1);
+									} })
+		},
 		
+		test2(){
+			console.log(this.apartment);
+		},
+		
+		uploadImage(index){
+			
+				fd = new FormData();
+				fd.append('image', this.files[index])
+				axios
+					.post('rest/apartments/images', fd)
+					.then((response) => {this.apartment.images.push(response.data); 
+										 if(++index < this.files.length){ 
+											 this.uploadImage(index);
+										 }else{
+											 console.log(this.apartment);
+											 axios
+												.post('rest/apartments', this.apartment)
+												.then((response) => {console.log(response.data)})
+										 }
+										 })
+		},
+		
+		imageAdded(event){
+			this.files = event.target.files;
+			this.listKey += 1;
+		},
+		
+		removeImage(file){
+			for(i = 0, j = 0; i < this.files.length; ++i){
+				if(file.name !== this.files[i].name){
+					this.files[j++] = this.files[i];
+				}
+			}
+		},
+		
+		addApprovedDate(){
+			var l = moment(this.approvedDate.start, 'DD/MM/YYYY');
+			var r = moment(this.approvedDate.end, 'DD/MM/YYYY');
+			this.apartment.approvedDates.push({l, r});
+			console.log(this.apartment.approvedDates)
+		},
+		
+		addAmenity(){
+			this.addedAmenities.push(this.selectedAmenity);
+			this.apartment.amenitiesIds.push(this.selectedAmenity.id)
+			for(i = 0; i < this.amenities.length; ++i){
+				if(this.amenities[i].id === this.selectedAmenity.id){
+					this.amenities.splice(i,1);
+				}
+			}
+		},
+		
+		removeAmenity(){
+			this.amenities.push(this.selectedAmenity);
+			for(i = 0; i < this.addedAmenities.length; ++i){
+				if(this.addedAmenities[i].id === this.selectedAmenity.id){
+					this.addedAmenities.splice(i,1);
+					this.apartment.amenitiesIds.splice(i,1);
+				}
+			}
 		}
+	},
+	filters: {
+    	dateFormat: function (value, format) {
+    		var parsed = moment(value);
+    		return parsed.format(format);
+    	}
 	}
 })
